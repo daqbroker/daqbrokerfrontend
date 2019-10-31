15bb01047adb539f6e206478f8708a19ba702cd4
Antonio <amcbd89@gmail.com>
2019-10-31 09:07:31 +0000

    Test changes for submodules

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="WELCOME - HOME" />
+    <HelloWorld msg="WELCOME - HOME - TEST2" />
   </div>
 </template>
 
